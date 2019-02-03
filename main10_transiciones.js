557312fd8a8056ef830f12175ff110cf060e5488
gustavol58 <tavohen@gmail.com>
2019-02-03 14:35:55 -0500

    primer commit

---
+++ main10_transiciones.js
@@ -0,0 +1,16 @@
+// vue.js video 10: Transiciones cuando se ponen/quitan elementos del DOM
+
+const vm = new Vue({
+    el: 'main',
+    data: {
+          titulo: 'Tablas de multiplicar',
+          tabla:2,
+          limite:9,
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+});
